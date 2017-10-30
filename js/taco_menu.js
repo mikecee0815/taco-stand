47c19a39ae81a776b592613bdb5523dbbe544a84
mike <mikecee0815@gmail.com>
2017-10-30 18:37:52 -0400

    can make ajax calls and display menus

--- js/taco_menu.js
+++ js/taco_menu.js
@@ -20,6 +20,24 @@ var menus;                                      // Declare global variable
 
   loadMenu();
 
+  $('#main').on('click', 'a', function(e){
+    e.preventDefault()
+    var menuId = this.id.toUpperCase(); // Get value of id attr 
+    var newContent="";
+    for (var i = 0; i < menus[menuId].length; i++) {      // looping through menu
+      // newContent+='<span>' + menus[menu][i].price + '</span>';
+      newContent+= '<li class="list-item my-2">';
+      newContent += '<a class="mr-4 mt-3 text-capitalize " href="details.html#';
+      newContent += menus[menuId][i].title.replace(/ /g, '-') + ' ">';
+      newContent += menus[menuId][i].title + ' </a></li>';
+}
+      $('#menu-display').html('<ul class="ul-list">' + newContent + '</ul>');
+    
+    // console.log(menuId);
+
+
+  });
+
 
 
 
